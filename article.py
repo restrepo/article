3b1bdae913dc1fd4f8995b7131f81a677d9aae46
Diego Restrepo <restrepo@udea.edu.co>
2016-02-09 23:57:28 -0500

    Impact factor

--- article.py
+++ article.py
@@ -12,6 +12,43 @@ pd.set_option('display.max_rows', 500)
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.max_colwidth',500)
 
+def google_search(q='hello world'):
+    '''
+    From: http://stackoverflow.com/a/29292168
+    '''
+    rd={}
+    link = 'http://ajax.googleapis.com/ajax/services/search/web?v=1.0&%s' %q
+    ua = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2)\
+           AppleWebKit/537.36 (KHTML, like Gecko) Chrome/27.0.1453.116 Safari/537.36'}                                                                
+    payload = {'q': q}                                                                                                                                                                                   
+    r = requests.get(link, headers=ua, params=payload)
+    if r.status_code==200:
+        rd=r.json()
+    return rd
+    
+def ImpactFactor(issn='1539-3755'):
+    ifdf=pd.DataFrame()
+    q='site:www.bioxbio.com/if/ %s' %issn
+    rd=google_search(q)
+    
+    IF=''
+    if rd:
+        if rd.has_key('responseData'): # if exits, is a dict
+            if rd['responseData'].has_key('results'): 
+                if len(rd['responseData']['results'])>0:
+                    if rd['responseData']['results'][0].has_key('content'):
+                        IF=rd['responseData']['results'][0]['content']
+        
+        if IF:
+            IF=IF.replace('\n','').split('impact factors:')
+            if len(IF)>0:
+                IF=IF[1].split('. ISSN')[0].split(',') #always works for a string
+                IF=[ify.replace(')','').split('(') for ify in  IF]
+                if len(IF[0])==2:
+                    ifdf=pd.DataFrame(IF,columns=['IF','Year'])
+                    
+    return ifdf
+
 def keysdash2underscore(ds):
     '''
     Convert keys with dash  to keys with underscore
@@ -29,8 +66,13 @@ class article(object):
     urldoi='http://dx.doi.org/'
     citedby=pd.Series()
     json=pd.Series()
-    def __init__(self,doi='10.1007/JHEP11(2013)011',citations=True,json=True,colciencias=True):
+    def __init__(self,doi='10.1007/JHEP11(2013)011',citations=True,json=True,colciencias=False,\
+                 impact_factor=True):
+        if impact_factor and not colciencias:
+            json=True
+            
         self.doi=doi.replace(self.urldoi,'')
+        self.issn=''
         #print 'TODO: JSON metadata here'
         
         if citations:
@@ -62,7 +104,12 @@ class article(object):
                             if self.json[k]:
                                 self.json[k]=self.json[k][0]
                 #proper sub-Series for nested json keys...
-
+        if not colciencias:
+            if json:
+                self.issn=self.json.ISSN[0]
+        #else: force issn colciencias
+        if impact_factor:
+            self.impact_factor=ImpactFactor(self.issn)
 if __name__ == "__main__":                
     a=article(doi='http://dx.doi.org/10.1103/PhysRevD.92.013005')
     print a.citedby.number
